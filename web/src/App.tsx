3091e04c1d9ea03c661ce0ce62cf2e3a0b629e23
Daniel Wzoreck <wzoreck2000@gmail.com>
2022-09-12 20:28:57 -0300

    Configuracao e apresentacao incial do React finalizada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,21 +3,32 @@
 // React é um conglomerado de funções que retornam HTML, CSS e JS!
 
 /* Principais conceitos do React Componentes / Propriedades
-   Componente: é uma função que retorna um conjunto de tags HTML
-   Propriedade:
+   Componente: é uma função que retorna um conjunto de tags HTML, podem ser reaproveitados.
+   Propriedade: permite trazer diferênciação entre cada 
 
 
 */
 
-function Button() {
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
   return (
-    <button>Enviar</button>
+    <button>{props.title}</button> // Se usa chaves quando quer incluir JS em um componente HTML
   )
 }
 
 function App() {
   //return <h1>Hello World</h1>
-  return <Button />
+  return (
+    <div>
+      <Button title="Enviar 1" />
+      <Button title="Enviar 2" />
+      <Button title="Enviar 3" />
+      <Button title="Enviar 4" />
+    </div>
+  )
 }
 
 export default App
