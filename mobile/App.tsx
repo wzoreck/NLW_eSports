aba6691177f5bad0799b3a1551890ec7c784ae04
Daniel Wzoreck <wzoreck2000@gmail.com>
2022-09-21 10:44:37 -0300

    Aprendendo a estrutura basica do projeto e criando o primeiro componente

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Hello World
+      </Text>
+
+      <Button1 title="Botão 1" />
+      <Button1 title="Botão 2" />
+      <Button1 title="Botão 3" />
+      <Button1 title="Botão 4" />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Criando um componente
+interface ButtonProps {
+  title: string;
+}
+
+function Button1(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+// Aqui fica a estilização CSS, o container está sendo chamado em <View style={styles.container}>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
