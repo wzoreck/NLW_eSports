fa4081b850389237e9928a8d141c636527fbe31b
Daniel Wzoreck <wzoreck2000@gmail.com>
2022-09-25 18:59:43 -0300

    Adicionado cards com descricao para os jogos e div em baixo para encontrar duo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,6 +9,8 @@
    Propriedade: permite trazer diferênciação entre cada componente
 */
 
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
@@ -22,6 +24,73 @@ function App() {
       <h1 className='text-6xl text-white font-black mt-20'>
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text '>duo</span> está aqui
       </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+       
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+          
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/cs_go.jpg" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+      </div>
+      
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400'>Publique um anúncio para encontrar players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded-lg flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio 
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
@@ -33,4 +102,7 @@ export default App
 - Instalar Tail Wind CSS (https://tailwindcss.com)
   npm install -D tailwindcss postcss autoprefixer
   npx tailwindcss init -p
+
+- Icones phosphor
+  npm install phosphor-react
 */
